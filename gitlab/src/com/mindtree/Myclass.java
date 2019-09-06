b7f4b69bc3b996a9ece6ab34ac55b8f8bf29d71b
Harini Mani <M1053644@mindtree.com>
2019-09-06 12:56:15 +0530

    Added harini

--- gitlab/src/com/mindtree/Myclass.java
+++ gitlab/src/com/mindtree/Myclass.java
@@ -5,6 +5,7 @@ public class Myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Added subha");
+System.out.println("Added harini");
 	}
 
 }
